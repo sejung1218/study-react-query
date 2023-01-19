3bb2da98e23d00bfc6676f77e4b862fc98712cd4
unknown <sejung1218@hanmail.net>
2023-01-19 17:35:55 +0900

    create Example components

--- src/app/test01/page.tsx
+++ src/app/test01/page.tsx
@@ -1,28 +1,13 @@
 "use client";
+import { Example } from "@/components/Example";
 import router from "next/router";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
 export default function TestPage01() {
-  const { status, data, error } = useQuery("todos", fetchTodoList);
-
-  if (status === "loading") {
-    return <span>Loading...</span>;
-  }
-
-  if (status === "error") {
-    return <span>Error: {error.message}</span>;
-  }
-
+  const queryClient = new QueryClient();
   return (
-    <div>
-      <div>Hello</div>
-      <div>It's me</div>
-      <div>First Page</div>
-      <ul>
-        {data.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   );
 }
