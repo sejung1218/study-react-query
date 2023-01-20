389c35f88e1203050d98fda08bdef3e11b9f1f13
unknown <sejung1218@hanmail.net>
2023-01-20 14:33:38 +0900

    resolve data error

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -9,9 +9,14 @@ export function Example() {
 
   console.log("repoData : ", data?.repoData);
 
-  if (isLoading) return "Loading...";
+  // if (isLoading) return "Loading...";
+  if (isLoading) return <div>"Loading...";</div>;
 
-  if (error) return "An error has occurred: " + error;
+  // 이렇게 작성하면 상위에서 'Example'은(는) JSX 구성 요소로 사용할 수 없습니다.
+  // 해당 반환 형식 'string | Element'은(는) 유효한 JSX 요소가 아닙니다. 라는 에러 출력.
+  // if (error) return "An error has occurred: " + error;
+  // 아래와 같이 작성해야 한다. feat. 제호
+  if (error) return <div>"An error has occurred: " + error;</div>;
 
   return (
     <div>
