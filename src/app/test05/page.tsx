2070a1e58e531327d7fa08d63a3f4636c9731982
sejung1218 <sejung1218@hanmail.net>
2023-03-01 14:50:05 +0900

    Change Repository

--- src/app/test05/page.tsx
+++ src/app/test05/page.tsx
@@ -1,10 +1,5 @@
 "use client";
-import { TestReactQuery01 } from "@/components/TestReactQuery01";
-import { TestReactQuery02 } from "@/components/TestReactQuery02";
-import { TestReactQuery02_01 } from "@/components/TestReactQuery02_01";
-import { TestReactQuery02_02 } from "@/components/TestReactQuery02_02";
-import { TestReactQuery03 } from "@/components/TestReactQuery03";
-import { TestReactQuery04 } from "@/components/TestReactQuery04";
+
 import router from "next/router";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
@@ -19,12 +14,7 @@ export default function TestPage01() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <TestReactQuery01 /> */}
-      {/* <TestReactQuery02 /> */}
-      {/* <TestReactQuery02_01 /> */}
-      {/* <TestReactQuery02_02 /> */}
-      {/* <TestReactQuery03 /> */}
-      <TestReactQuery04 />
+      Test
     </QueryClientProvider>
   );
 }
